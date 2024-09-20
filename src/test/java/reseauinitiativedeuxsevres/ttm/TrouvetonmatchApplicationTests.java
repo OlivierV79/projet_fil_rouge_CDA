0c74749676f76593c06bf1acb34eacec36cfabff
VILLAVERDE Olivier <olivier.villaverde@maif.fr>
2024-09-20 11:39:33 +0200

    init Spring

---
+++ src/test/java/reseauinitiativedeuxsevres/ttm/TrouvetonmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package reseauinitiativedeuxsevres.ttm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrouvetonmatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
