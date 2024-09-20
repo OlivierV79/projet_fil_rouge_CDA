90d14294a9ec631c36db3248fd07703b636d1959
VILLAVERDE Olivier <olivier.villaverde@maif.fr>
2024-09-20 11:49:06 +0200

    modif parametres de spring

--- src/main/java/reseauinitiativedeuxsevres/ttm/TrouvetonmatchApplication.java
+++ src/main/java/reseauinitiativedeuxsevres/ttm/TrouvetonmatchApplication.java
@@ -2,8 +2,9 @@ package reseauinitiativedeuxsevres.ttm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TrouvetonmatchApplication {
 
     public static void main(String[] args) {
